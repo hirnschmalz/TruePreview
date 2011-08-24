d287d5b0d7119ed8aba00a6e1fad9bf34751b40b
jhriggs <jhriggs@mycompany.com>
2011-08-24 03:20:01 +0000

    1.8a1
    git-svn-id: http://christianserving.org/svn/truepreview/trunk@54 b2f35b9b-6b25-4758-a704-3800dd0c7bf6

--- Installer/postinstall.sh
+++ Installer/postinstall.sh
@@ -30,15 +30,11 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ##
-# postflight.sh
+# postinstall.sh
 # Copyright (c) 2009-2011 Jim Riggs, Christian Serving, L.L.C. All rights reserved.
 #
-# @(#) $Id$
+# @(#) $Id: postflight.sh 53 2011-08-23 16:00:04Z jhriggs $
 ##
 
-defaults write "$HOME/Library/Preferences/com.apple.mail" EnableBundles -bool YES
-defaults write "$HOME/Library/Preferences/com.apple.mail" BundleCompatibilityVersion -int 4
-chown "$USER" "$HOME/Library/Preferences/com.apple.mail."*
-chown -R "$USER" "$HOME/Library/Mail/Bundles"
-
-rm -f "/private/tmp/TruePreviewInstallerCurrentUser"
+defaults write "com.apple.mail" EnableBundles -bool YES
+defaults write "com.apple.mail" BundleCompatibilityVersion -int 4
